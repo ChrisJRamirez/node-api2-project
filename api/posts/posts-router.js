6cb8305224b62f1358eee8b2e56803ec45d482a0
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-05 18:33:26 -0500

    setting up project and server

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,8 @@
 // implement your posts router here
+const Post = require("./posts-model");
+const express = require("express");
+const router = express.Router();
+
+//POSTS Endpoints
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
+const express = require("express");
+
 // require your posts router and connect it here
+const postsRouter = require("./api/posts/posts-router");
+
+const server = express();
+
+server.use(express.json());
+server.unsubscribe("/api/posts", postsRouter);
+
+// Catch-All Endpoint
+server.get("/", (req, res) => {
+  res.send(`
+  <h2> Catch-All Endpoint has been hit </h2>`)
+});
+
+module.exports = server;
