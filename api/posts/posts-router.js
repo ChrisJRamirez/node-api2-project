4abdd4924f54b82ba2fb191d1a2c446c29cec63e
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-05 20:48:50 -0500

    added in PUT endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -34,6 +34,45 @@ router.get("/:id", (req, res) => {
         message: "The post information could not be retrieved"
       })
     })
+});
+
+// 3 [POST] /api/posts
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  if(!newPost.title || !newPost.contents) {
+    res.status(400).json({message: "Please provide title and contents for the post"})
+  } else {
+    Post.insert(newPost)
+      .then(post => {
+        res.status(201).json(post)
+      })
+      .catch(err => {
+        res.status(500).json({message: "There was an error while saving the post to the database"})
+      })
+  }
+});
+
+// 4 [PUT] /api/posts/:id
+router.put("/:id", async (req, res) => {
+  const changes = req.body;
+  const {id} = req.params;
+
+  try{
+    if(!changes.title || !changes.contents){
+      res.status(400).json({message: "Please provide title and contents for the post"})
+    } else {
+      const updatedPost = await Post.update(id, changes)
+      if(!updatedPost){
+        res.status(404).json({message: "The post with the specified ID does not exist"})
+      }else {
+        res.status(200).json(updatedPost)
+      }
+    }
+  } catch(err){
+    res.status(500).json({message: "The post information could not be modified"})
+  }
+
+
 })
 
 module.exports = router;
